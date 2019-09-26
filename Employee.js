ebe976d6cc8c47bb4fe7cd8348a9c8951bfb381f
Zennen <nerfeboi@users.noreply.github.com>
2019-09-26 14:04:34 +0800

    Update Employee.js

--- Employee.js
+++ Employee.js
@@ -1,5 +1,17 @@
 class Employee{
 var firstName;
 var lastName;
-int age;
+var age;
+  Employee(firstName, lastName, age){
+    this.firstName=firstName;
+    this.lastName=lastName;
+    this.age=age;
+  }
+  getCompleteName(){
+    var completeName = '';
+    if(this.firstName!='' && this.lastName!=''){
+      completeName = this.firstName + ' ' + this.lastName;
+    }
+    return completeName;
+  }
 }
